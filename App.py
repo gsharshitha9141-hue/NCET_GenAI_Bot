29fa986a06ce46ea04509995ae703e738585558e
gsharshitha9141-hue <gsharshitha9141@gmail.com>
2026-04-15 10:35:18 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from groq import Groq
 
-st.set_page_config("PragyanAI Context Generator",layout="wide")
-st.title("PragyanAI_Transperent.jpg")
+st.set_page_config("SugnyanAI Context Generator",layout="wide")
+st.title("https://github.com/gsharshitha9141-hue/NCET_GenAI_Bot/blob/main/Screenshot_20241027_201934%5B1%5D.jpg")
